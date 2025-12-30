ae4faf6b263f5026dc3911ac327271e4d457260b
AMT-Technology <des.amt.01@gmail.com>
2025-12-29 20:06:31 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCEMOAiEhDC-Z5nS4_yB-a1s3T_61PrHF4"
   authDomain: "appwebs-8b23f.firebaseapp.com",
   projectId: "appwebs-8b23f",
-  storageBucket: "appwebs-8b23f.firebasestorage.app",
+  storageBucket: "appwebs-8b23f.firebasestorage.com",
   messagingSenderId: "986164552498",
   appId: "1:986164552498:web:04262bde6fa35e5efced2b",
 };
